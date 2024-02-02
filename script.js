1f98adb1ad87acb27bc3ca307632b1a3b80072d3
Trine <trla0001@stud.kea.dk>
2024-02-02 11:13:39 +0100

    De klappede

--- script.js
+++ script.js
@@ -32,13 +32,16 @@ function init() {
 }
 
 function showBeers(beerJSON) {
-  console.log("Første json element", beerJSON[0]);
+  let beerClone;
 
-  let beerClone = beerTemplate.cloneNode(true).content;
-  beerClone.querySelector(".beer_image").src = beerJSON[0].image_url;
-  beerClone.querySelector(".beer_image").alt = `Picture of a ${beerJSON[0].name} beer`;
-  beerClone.querySelector(".beer_name").textContent = beerJSON[0].name;
-  beerClone.querySelector(".beer_tagline").textContent = beerJSON[0].tagline;
-  beerClone.querySelector(".beer_description").textContent = beerJSON[0].description;
-  beerContainer.appendChild(beerClone);
+  beerJSON.forEach((beer) => {
+    console.log("Beer", beer);
+    beerClone = beerTemplate.cloneNode(true).content;
+    beerClone.querySelector(".beer_image").src = beer.image_url;
+    beerClone.querySelector(".beer_image").alt = `Picture of a ${beer.name} beer`;
+    beerClone.querySelector(".beer_name").textContent = beer.name;
+    beerClone.querySelector(".beer_tagline").textContent = beer.tagline;
+    beerClone.querySelector(".beer_description").textContent = beer.description;
+    beerContainer.appendChild(beerClone);
+  });
 }
