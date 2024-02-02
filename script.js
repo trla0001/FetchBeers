9f7cbb0b1816ca985d0302e9ff5f487823fba8ca
Trine <trla0001@stud.kea.dk>
2024-02-02 10:14:53 +0100

    template added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Lets go");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+
+/* json kaldes og laves om. response er mellemtrin, som laves om til json. return response = retunerer json*/
+function init() {
+  fetch(beerURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
