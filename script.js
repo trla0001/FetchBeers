fd70168ed07ee227dfeda15d0f4ec20c53020f77
Trine <trla0001@stud.kea.dk>
2024-02-02 10:28:34 +0100

    fetch

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ window.addEventListener("DOMContentLoaded", init);
 const beerURL = "https://api.punkapi.com/v2/beers";
 
 /* json kaldes og laves om. response er mellemtrin, som laves om til json. return response = retunerer json*/
+
+/* alt inde i fetch er forsinket, og udenfor er klar. Derfor hedder det asynkron */
+
 function init() {
   fetch(beerURL)
     .then(function (response) {
@@ -10,5 +13,10 @@ function init() {
     })
     .then(function (json) {
       console.log("json", json);
+      showBeers(json);
     });
+}
+
+function showBeers(beerJSON) {
+  console.log("Første json element", beerJSON[0]);
 }
